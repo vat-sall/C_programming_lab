f80c2448736197a759545144d7fe4406e25fc2ab
VATSAL <146457829+vatsal038@users.noreply.github.com>
2023-10-04 18:53:07 +0530

    Update 3.2 Program to check whether the alphabet is a vowel or a consonant.c

--- 3.2 Program to check whether the alphabet is a vowel or a consonant.c
+++ 3.2 Program to check whether the alphabet is a vowel or a consonant.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-int main() {
-    char c;
-    int lowercase_vowel, uppercase_vowel;
-    printf("Enter an alphabet: ");
-    scanf("%c", &c);
 
-    
-    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
+int main()
+{
+    char value;
+    printf("Enter the Character :-");
+    scanf("%c%c", &value,&value);
+    if (value == 'a'||value == 'e'||value == 'i'||value == 'o'||value == 'u'||value == 'A'||value =='E'||value == 'I'||value == 'O'||value =='U')printf("Vowel");
+    else printf("Consonant");
 
-   
-    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-
-    
-    if (lowercase_vowel || uppercase_vowel)
-        printf("%c is a vowel.", c);
-    else
-        printf("%c is a consonant.", c);
     return 0;
 }
