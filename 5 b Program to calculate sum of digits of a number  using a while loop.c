0058e45c4d61dcdc32a470ad4f76305cf9b8e10d
VATSAL <146457829+vatsal038@users.noreply.github.com>
2023-10-18 19:57:10 +0530

    Update 5 b Program to calculate sum of digits of a number  using a while loop.c

--- 5 b Program to calculate sum of digits of a number  using a while loop.c
+++ 5 b Program to calculate sum of digits of a number  using a while loop.c
@@ -3,7 +3,7 @@
 void main()
 {
     int num,rem,sum=0;
-    printf("Enter the number :- ");
+    printf("Enter the number : ");
     scanf("%d",&num);
     while(num)
     {
@@ -11,6 +11,6 @@ void main()
         sum = sum + rem;
         num/=10;
     }
-    printf("Sum of digits are :- %d ",sum);
+    printf("Sum of digits are : %d ",sum);
     
 }
